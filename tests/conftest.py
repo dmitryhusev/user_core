39b2583bd683d1c917077badebe5cc145ca8894d
dmitryhusev <dimtri.gusev@gmail.com>
2023-10-08 17:08:00 +0200

    allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,29 @@
 import pytest
 
+class RerunErrorHandler:
+        record_trace = False
+        test_name = None
+        count = 0
+
+handler = RerunErrorHandler()
+
+def pytest_exception_interact(call, report):
+    handler.test_name = report.head_line
+    handler.record_trace = True
+    handler.count += 1
+
 
 @pytest.fixture()
 def page(playwright, request):
     browser = playwright.chromium.launch()
     context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    if handler.record_trace:
+        if request.node.name == handler.test_name:
+            context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
     yield page
-    context.tracing.stop(path=f"./traces/{request.node.name}.zip")
+    if (
+         handler.record_trace and
+         handler.test_name == request.node.name and
+         handler.count > 1):
+            context.tracing.stop(path=f"./traces/{request.node.name}.zip")
