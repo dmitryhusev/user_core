d72a7f977dd8cf7722471d93c880b643c8910eca
dmitryhusev <dimtri.gusev@gmail.com>
2023-10-22 02:51:47 +0200

    upd traces handling

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 import pytest
 from user_core.utils.tests import RerunErrorHandler
+import os
+
+
+RERUNS = os.getenv('RERUNS', 1)
 
 handler = RerunErrorHandler()
-
 
 def pytest_exception_interact(report):
     handler.record_trace(report.head_line)
@@ -12,13 +15,11 @@ def pytest_exception_interact(report):
 def page(playwright, request):
     browser = playwright.chromium.launch()
     context = browser.new_context()
-    if handler.is_failed:
+    if handler.is_failed and handler.count == RERUNS:
         if request.node.name == handler.test_name:
             context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
     yield page
-    if (
-         handler.is_failed and
-         handler.test_name == request.node.name and
-         handler.count > 1):
-            context.tracing.stop(path=f"./traces/{request.node.name}.zip")
+    if handler.count == RERUNS + 1:
+        handler.reset_state()
+        context.tracing.stop(path=f"./traces/{request.node.name}.zip")
--- user_core/utils/tests.py
+++ user_core/utils/tests.py
@@ -4,6 +4,9 @@ class RerunErrorHandler:
         # relevant when tests are running with --reruns flag
         
         def __init__(self):
+            self.init_state()
+
+        def init_state(self):
             self.test_name = ''
             self.is_failed = False
             self.count = 0
@@ -12,3 +15,6 @@ class RerunErrorHandler:
             self.test_name = test_name
             self.is_failed = True
             self.count += 1
+
+        def reset_state(self):
+            self.init_state()
